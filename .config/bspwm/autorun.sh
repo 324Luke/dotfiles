7a05b116077c4422845b516903b675fbe0554a9b
Luke Whrit <lukewhrit@gmail.com>
2020-08-16 15:11:12 -0400

    1597605005

---
+++ .config/bspwm/autorun.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+## run (only once) processes which spawn with the same name
+function run {
+  if (command -v $1 && ! pgrep $1); then
+    $@&
+  fi
+}
+
+## run (only once) processes which spawn with different name
+if (command -v gnome-keyring-daemon && ! pgrep gnome-keyring-d); then
+  gnome-keyring-daemon --daemonize --login &
+fi
+
+if (command -v start-pulseaudio-x11 && ! pgrep pulseaudio); then
+  start-pulseaudio-x11 &
+fi
+
+if (command -v /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 && ! pgrep polkit-mate-aut) ; then
+  /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+fi
+
+run xfsettingsd
+run nm-applet
+run light-locker
+run xcape -e 'Super_L=Super_L|Control_L|Escape'
+run feh --bg-scale pics/Wallpapers/wallpaper.jpg
+run pa-applet
+run picom --shadow-exclude '!focused'
+run blueman-applet
+run msm_notifier
