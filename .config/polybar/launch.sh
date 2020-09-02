9d2b00c00dcdaafde673156fb46d1c9865a92402
Luke Whrit <lukewhrit@gmail.com>
2020-09-02 14:44:51 -0400

    1599072227

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -c $HOME/.config/polybar/config.ini main &
-
-echo "Bars launched..."
+polybar -c $HOME/.config/polybar/config.ini default &
